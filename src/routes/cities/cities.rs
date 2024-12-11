587a1a1adfc2d7669e01b469c015936750228cb8
mich1991 <wozniak.michal91@gmail.com>
2024-12-11 17:26:33 +0000

    new city routes

--- src/routes/cities/cities.rs
+++ src/routes/cities/cities.rs
@@ -1,5 +1,5 @@
 use crate::AppState;
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder, post};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
@@ -25,6 +25,51 @@ pub async fn get_cities(state: web::Data<AppState>) -> impl Responder {
     }
 }
 
+#[get("/{country_id}")]
+pub async fn get_cities_by_country(
+    state: web::Data<AppState>,
+    path: web::Path<i16>,
+) -> impl Responder {
+    let country_id = path.into_inner();
+    match sqlx::query_as::<_, City>(
+        "SELECT * FROM city WHERE country_id = $1",
+    )
+    .bind(country_id)
+    .fetch_all(&state.db)
+    .await
+    {
+        Ok(cities) => HttpResponse::Ok().json(cities),
+        Err(e) => {
+            println!("{e}");
+            HttpResponse::NotFound().body("Cities not found")
+        }
+    }
+}
+
+#[post("")]
+pub async fn post_city(
+    state: web::Data<AppState>,
+    city: web::Json<City>,
+) -> impl Responder {
+    match sqlx::query(
+        "INSERT INTO city (city, country_id, last_update) VALUES ($1, $2, $3)",
+    )
+    .bind(&city.city)
+    .bind(&city.country_id)
+    .bind(&city.last_update)
+    .execute(&state.db)
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().body("City added successfully"),
+        Err(e) => {
+            println!("{e}");
+            HttpResponse::NotFound().body("City not added")
+        }
+    }
+}
+
+
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(get_cities);
+    cfg.service(get_cities_by_country);
 }
